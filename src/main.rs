e65ea8a367339ecbdc4ca46e4d7bcf4f1046a05c
Alexandru Olaru <alxolr@gmail.com>
2020-05-21 11:38:54 +0300

    Added structopt enums to my command line arguments

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,55 @@
 mod version_utils;
+use structopt::StructOpt;
 
 use std::error::Error;
 use std::fs::OpenOptions;
 use std::io::{BufReader, Read, Write};
+use std::path::PathBuf;
 use std::process;
 
+#[derive(StructOpt, Debug)]
+#[structopt(
+    about = "Productivity scripts for deployment",
+    rename_all = "kebab-case"
+)]
+enum Drunx {
+    Patch {
+        #[structopt(help = "Sprint number used in the created tag")]
+        sprint: i32,
+        #[structopt(
+            short = "d",
+            long = "dry_run",
+            help = "Run the steps without executing"
+        )]
+        dry_run: bool,
+        #[structopt(
+            short = "p",
+            long = "path",
+            help = "Provide the path for the node project",
+            default_value = "."
+        )]
+        path: PathBuf,
+    },
+}
+
 fn run() -> Result<(), Box<dyn Error>> {
-    let file = OpenOptions::new().read(true).open("data/package.json")?;
-    let mut buf_reader = BufReader::new(file);
-    let mut contents = String::new();
-    buf_reader.read_to_string(&mut contents)?;
+    let drunx = Drunx::from_args();
+    println!("{:?}", drunx);
 
-    let version = version_utils::find(&contents);
-    if version.is_some() {
-        contents = contents.replace(&version.unwrap(), "3.0.0a");
-    }
+    // let file = OpenOptions::new().read(true).open("data/package.json")?;
+    // let mut buf_reader = BufReader::new(file);
+    // let mut contents = String::new();
+    // buf_reader.read_to_string(&mut contents)?;
 
-    let mut file = OpenOptions::new().write(true).open("data/package.json")?;
+    // let version = version_utils::find(&contents);
+    // if version.is_some() {
+    //     contents = contents.replace(&version.unwrap(), "3.0.0a");
+    // }
 
-    file.write_all(contents.as_bytes())?;
-    file.sync_all()?;
+    // let mut file = OpenOptions::new().write(true).open("data/package.json")?;
+
+    // file.write_all(contents.as_bytes())?;
+    // file.sync_all()?;
 
     Ok(())
 }
