393a130e86aac2ccd60370bdb9264e0d32a94dde
Alexandru Olaru <alxolr@gmail.com>
2020-06-03 23:28:37 +0300

    Added the module to compute patch version

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+mod patch_utils;
 mod version;
 mod version_utils;
 
---
+++ src/patch_utils/mod.rs
@@ -0,0 +1,50 @@
+pub fn compute_patch(release: &str) -> String {
+  let mut items = release.chars().collect::<Vec<char>>();
+  let last = items.last().unwrap();
+
+  if last.is_digit(10) {
+    items.push('a');
+  } else {
+    if last == &'z' {
+      let size = items.len();
+      items[size - 1] = 'a';
+      items.push('a');
+    } else {
+      let last_as_number = *last as u32;
+      let next_letter = std::char::from_u32(last_as_number + 1).unwrap();
+
+      let size = items.len();
+      items[size - 1] = next_letter;
+    }
+  }
+
+  items.into_iter().collect::<String>()
+}
+
+#[cfg(test)]
+mod tests {
+  use super::*;
+  #[test]
+  fn should_create_a_version_if_first() {
+    let release = "1.0.0";
+    let expected = "1.0.0a".to_string();
+
+    assert_eq!(compute_patch(release), expected);
+  }
+
+  #[test]
+  fn should_add_patch_version_b_if_a() {
+    let release = "1.0.0a";
+    let expected = "1.0.0b".to_string();
+
+    assert_eq!(compute_patch(release), expected);
+  }
+
+  #[test]
+  fn for_the_case_of_last_letter_z_add_za() {
+    let release = "1.0.0z";
+    let expected = "1.0.0aa".to_string();
+
+    assert_eq!(compute_patch(release), expected);
+  }
+}
