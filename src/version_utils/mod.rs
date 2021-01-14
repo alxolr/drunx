eeb25bb97b4225c995e96f91a15b513718484b40
Alexandru Olaru <alxolr@gmail.com>
2021-01-14 15:27:31 +0200

    Possible fix for adding a broken } when replacing version

--- src/version_utils/mod.rs
+++ src/version_utils/mod.rs
@@ -5,89 +5,89 @@ use std::io::{BufReader, Read, Write};
 use std::path::PathBuf;
 
 pub fn find(contents: &str) -> Option<String> {
-  let re = Regex::new(r#""version":\s"([0-9a-z\.]+)""#).unwrap();
-  let capts = re.captures(contents);
+    let re = Regex::new(r#""version":\s"([0-9a-z\.]+)""#).unwrap();
+    let capts = re.captures(contents);
 
-  if capts.is_some() {
-    Some(capts.unwrap()[1].to_string())
-  } else {
-    None
-  }
+    if capts.is_some() {
+        Some(capts.unwrap()[1].to_string())
+    } else {
+        None
+    }
 }
 
 pub fn get_version_from_file(file_path: &PathBuf) -> Result<String, Box<dyn Error>> {
-  let file = OpenOptions::new().read(true).open(file_path.as_path())?;
-  let mut buf_reader = BufReader::new(file);
-  let mut contents = String::new();
-  buf_reader.read_to_string(&mut contents)?;
+    let file = OpenOptions::new().read(true).open(file_path.as_path())?;
+    let mut buf_reader = BufReader::new(file);
+    let mut contents = String::new();
+    buf_reader.read_to_string(&mut contents)?;
 
-  let version = find(&contents);
+    let version = find(&contents);
 
-  match version {
-    Some(version) => Ok(version),
-    None => panic!("Couldn't find the version in the file"),
-  }
+    match version {
+        Some(version) => Ok(version),
+        None => panic!("Couldn't find the version in the file"),
+    }
 }
 
 pub fn change_version(file_path: &PathBuf, next_version: &str) -> Result<(), Box<dyn Error>> {
-  let file = OpenOptions::new().read(true).open(file_path.as_path())?;
+    let file = OpenOptions::new().read(true).open(file_path.as_path())?;
 
-  let mut buf_reader = BufReader::new(file);
-  let mut contents = String::new();
-  buf_reader.read_to_string(&mut contents)?;
+    let mut buf_reader = BufReader::new(file);
+    let mut contents = String::new();
+    buf_reader.read_to_string(&mut contents)?;
 
-  let version = find(&contents);
-  if version.is_some() {
-    contents = contents.replace(&version.unwrap(), next_version);
-  }
+    let version = find(&contents);
+    if version.is_some() {
+        contents = contents.replacen(&version.unwrap(), next_version, 2);
+    }
 
-  let mut file = OpenOptions::new().write(true).open(file_path.as_path())?;
+    let mut file = OpenOptions::new().write(true).open(file_path.as_path())?;
 
-  file.write_all(contents.as_bytes())?;
-  file.sync_all()?;
+    file.write_all(contents.as_bytes())?;
+    file.sync_all()?;
 
-  Ok(())
+    Ok(())
 }
 
 #[cfg(test)]
 mod find {
-  use super::find;
+    use super::find;
 
-  #[test]
-  fn extracts_the_version_if_exits() {
-    let content = r#"\
+    #[test]
+    fn extracts_the_version_if_exits() {
+        let content = r#"\
     {
         "name": "some_name",
         "version": "1.0.0",
     }"#;
 
-    assert_eq!(find(content).unwrap(), "1.0.0".to_string());
-  }
+        assert_eq!(find(content).unwrap(), "1.0.0".to_string());
+    }
 
-  #[test]
-  fn extracts_the_version_including_letters() {
-    let content = r#"\
+    #[test]
+    fn extracts_the_version_including_letters() {
+        let content = r#"\
     {
         "name": "some_name",
         "version": "1.0.0x",
     }"#;
 
-    assert_eq!(find(content).unwrap(), "1.0.0x".to_string());
-  }
+        assert_eq!(find(content).unwrap(), "1.0.0x".to_string());
+    }
 
-  #[test]
-  fn return_none_version_not_found() {
-    let content = r#"\
+    #[test]
+    fn return_none_version_not_found() {
+        let content = r#"\
     {
         "name": "some_name",
     }"#;
 
-    assert_eq!(find(content), None);
-  }
+        assert_eq!(find(content), None);
+    }
 
-  #[test]
-  fn extract_only_first_layer_of_version() {
-    let content = r#"\
+    #[test]
+    fn extract_only_first_layer_of_version() {
+        let content = r#"\
     {
         "name": "some_name",
         "version": "1.0.0x",
@@ -96,6 +96,6 @@ mod find {
         }
     }"#;
 
-    assert_eq!(find(content), Some(String::from("1.0.0x")));
-  }
+        assert_eq!(find(content), Some(String::from("1.0.0x")));
+    }
 }
